f7da35ac2b612c35a9c2c1aa966f9efb86821013
Bren Brightwell <bren@153.io>
2020-04-23 07:39:02 +0100

    add slot number arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 
 source $GITHUB_WORKSPACE/minecraft-tools/login.sh "$MINECRAFT_EMAIL" "$MINECRAFT_PASSWORD"
-$GITHUB_WORKSPACE/minecraft-tools/realms-download.sh
+$GITHUB_WORKSPACE/minecraft-tools/realms-download.sh $SLOT_NUMBER
 tar xvf world.tar.gz -C /home/minecraft/server
