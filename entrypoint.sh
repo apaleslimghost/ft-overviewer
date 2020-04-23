59a6399249fe41ded00eb6fc7365afadd1aa3ce3
Bren Brightwell <bren@153.io>
2020-04-23 07:47:58 +0100

    don't output my access token please lol

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-source $GITHUB_WORKSPACE/minecraft-tools/login.sh "$MINECRAFT_EMAIL" "$MINECRAFT_PASSWORD"
-$GITHUB_WORKSPACE/minecraft-tools/realms-download.sh $SLOT_NUMBER
+source $GITHUB_WORKSPACE/minecraft-tools/login.sh "$MINECRAFT_EMAIL" "$MINECRAFT_PASSWORD" > /dev/null
+$GITHUB_WORKSPACE/minecraft-tools/realms-download.sh "$SLOT_NUMBER"
 tar xvf world.tar.gz -C /home/minecraft/server
