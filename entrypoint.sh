f094e61d82f6de18c58d8a90405800ce86403d04
Bren Brightwell <bren@153.io>
2020-04-23 08:07:52 +0100

    inline the script, add more logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,50 @@
 
 set -e
 
-source $GITHUB_WORKSPACE/minecraft-tools/login.sh "$MINECRAFT_EMAIL" "$MINECRAFT_PASSWORD" > /dev/null
-$GITHUB_WORKSPACE/minecraft-tools/realms-download.sh "$SLOT_NUMBER"
-tar xvf world.tar.gz -C /home/minecraft/server
+auth_server=https://authserver.mojang.com
+client_token=github.com/air/minecraft-tools
+
+response=$(http --check-status --ignore-stdin POST ${auth_server}/authenticate username=${MINECRAFT_EMAIL} password=${MINECRAFT_PASSWORD} clientToken=${client_token} agent:='{"name": "Minecraft", "version": 1}')
+if [ $? -ne 0 ];then
+  echo "Error authenticating, response: ${response}"
+  return 1
+fi
+
+# parse details from the JSON response
+access_token=$(echo ${response} | jq -r .accessToken)
+name=$(echo ${response} | jq -r .selectedProfile.name)
+id=$(echo ${response} | jq -r .selectedProfile.id)
+
+echo "Logged in as $name"
+
+output_file=world.tar.gz
+realms_server=https://mcoapi.minecraft.net
+version=1.11.2
+
+cookie_string="Cookie:sid=token:${access_token}:${id};user=${name};version=${version}"
+
+# 1. get world ID
+response=$(http --check-status --ignore-stdin --verify=no GET ${realms_server}/worlds "${cookie_string}")
+if [ $? -ne 0 ];then
+  echo "Error getting worlds, response: ${response}"
+  exit 1
+fi
+world_id=$(echo ${response} | jq .servers[0].id)
+echo "Downloading from world $world_id"
+
+# 2. get download link
+response=$(http --check-status --ignore-stdin --verify=no GET ${realms_server}/worlds/${world_id}/slot/${SLOT_NUMBER}/download "${cookie_string}")
+if [ $? -ne 0 ];then
+  echo "Error getting download link, response: ${response}"
+  exit 2
+fi
+url=$(echo ${response} | jq -r .downloadLink)
+
+# 3. download backup
+http --check-status --ignore-stdin --body --download --verify=no --output ${output_file} ${url}
+if [ $? -ne 0 ];then
+  echo "Error downloading, exit code: $?"
+  exit 3
+fi
+
+tar xvf $output_file -C /home/minecraft/server
