6ee121aa2d5b2b2c97e6722a1d1860c5d8250b4c
Kara Brightwell <kara@153.io>
2020-09-23 18:22:40 +0100

    i guess it's an old wget?

--- actions/download/entrypoint.sh
+++ actions/download/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -m --ftp-username="$FTP_USER" --ftp-password="$FTP_PASS" "ftp://$FTP_HOST/$WORLD"
+wget -m --user="$FTP_USER" --password="$FTP_PASS" "ftp://$FTP_HOST/$WORLD"
 
 mv "$FTP_HOST/$WORLD" world
