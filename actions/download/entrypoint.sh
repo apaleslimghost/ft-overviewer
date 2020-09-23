07ab59bf023c7a065878a7fd5f25a20ec64cbf03
Kara Brightwell <kara@153.io>
2020-09-23 19:41:11 +0100

    me, i care about caching

--- actions/download/entrypoint.sh
+++ actions/download/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-wget -m --user="$FTP_USER" --password="$FTP_PASS" "ftp://$FTP_HOST/$WORLD"
-
-mv "$FTP_HOST/$WORLD" world
+lftp  -u"$FTP_USER,$FTP_PASS" "$FTP_HOST" -e "mirror --delete --only-newer $WORLD world ; quit"
